7531ea0cdac1160994f0e59fe93fa33d03a252ca
Fiachra Knox <fiachraknox@hotmail.com>
2017-12-08 13:37:40 +0000

    Remove duplicate generation functions from World

--- world.py
+++ world.py
@@ -10,61 +10,39 @@ class World:
   # Holds data on the world and the events within it
   # Methods allow listing of events nearby any given coordinate
 
-  def __init__(self, min_coords=(0, 0),
-               grid_data_source=(21, 21),
-               event_data_source=500):
+  def __init__(self, grid_data_source, event_data_source, min_coords=(0, 0)):
 
-    # Can either take a filename as a data source,
-    # or randomly generate testing data of given size.
+    # Takes filenames as data sources
     # Supported file types: csv, txt (treated as csv), json
 
     self.off = tuple([-x for x in min_coords])
-    event_nos = None
 
-    given_grid = isinstance(grid_data_source, str)
-    if given_grid:
-      ext = grid_data_source.split('.')[-1]
-      if ext in ['csv', 'txt']:
-        event_nos = self.load_grid_from_csv(grid_data_source)
-      elif ext == 'json':
-        with open(grid_data_source, 'r') as f:
-          self.grid = json.load(f)
-        event_nos = []
-        for row in self.grid:
-          for x in row:
-            if x is not None:
-              event_nos.append(x)
-      else:
-        raise UnrecognizedFormatError(
-          grid_data_source, 'is not in a recognized format.')
+    ext = grid_data_source.split('.')[-1]
+    if ext in ['csv', 'txt']:
+      event_nos = self.load_grid_from_csv(grid_data_source)
+    elif ext == 'json':
+      with open(grid_data_source, 'r') as f:
+        self.grid = json.load(f)
+      event_nos = []
+      for row in self.grid:
+        for x in row:
+          if x is not None:
+            event_nos.append(x)
     else:
-      if hasattr(grid_data_source, '__getitem__'):
-        self.size = grid_data_source
-      else:
-        raise UnrecognizedFormatError(
-          grid_data_source, 'is not subscriptable or a filename.')
+      raise UnrecognizedFormatError(
+        grid_data_source + ' is not in a recognized format.')
 
-    given_events = isinstance(event_data_source, str)
-    if given_events:
-      ext = event_data_source.split('.')[-1]
-      if ext in ['csv', 'txt']:
-        event_nos = self.load_events_from_csv(event_data_source)
-      elif ext == 'json':
-        with open(event_data_source, 'r') as f:
-          self.events = json.load(f)
-        self.event_nos = list(self.events.keys())
-      else:
-        raise UnrecognizedFormatError(
-          event_data_source, 'is not in a recognized format.')
 
-    if event_nos is None:
-      event_nos = random.sample(
-        range(10**9), k=min(self.size[0]*self.size[1], event_data_source))
-
-    if not given_grid:
-      self.generate_grid(event_nos)
-    if not given_events:
-      self.generate_tickets(event_nos)
+    ext = event_data_source.split('.')[-1]
+    if ext in ['csv', 'txt']:
+      event_nos = self.load_events_from_csv(event_data_source)
+    elif ext == 'json':
+      with open(event_data_source, 'r') as f:
+        self.events = json.load(f)
+      self.event_nos = list(self.events.keys())
+    else:
+      raise UnrecognizedFormatError(
+        event_data_source + ' is not in a recognized format.')
 
   def offset(self, x):
     return (x[0] + self.off[0], x[1] + self.off[1])
@@ -107,7 +85,6 @@ class World:
 
   def load_grid_from_csv(self, filename):
     # Returns the event numbers encoutered
-    # in case data needs to be generated for them
     self.grid = []
     event_nos = []
     row_length = None
@@ -135,7 +112,6 @@ class World:
     # Takes a CSV file with event id in the first column,
     # ticket prices and numbers available in the rest.
     # Returns the event numbers generated
-    # so that they are available for world generation
     self.events = {}
     with open(filename, 'r') as f:
       for line in f:
@@ -153,35 +129,3 @@ class World:
         self.events[event_no] = tickets
     return list(self.events.keys())
 
-  def generate_grid(self, event_nos):
-    # Randomly generates world data for testing purposes
-    idx = 0
-    # If we have very few events, spread them out well.
-    p = min(0.1, len(event_nos)/(self.size[0]*self.size[1]))
-    self.grid = []
-    for i in range(self.size[1]):
-      row = []
-      for j in range(self.size[0]):
-        if maybe(p) and idx < len(event_nos):
-          row.append(event_nos[idx])
-          idx += 1
-        else:
-          row.append(None)
-      self.grid.append(row)
-
-  def generate_tickets(self, event_nos):
-    # Randomly generates ticket price and availability data for testing
-    # Prices are stored in cents, not dollars!
-    # So we need to convert them back for the user
-    self.events = {}
-    for x in event_nos:
-      prices = []
-      for i in range(random.randint(0, 20)):
-        prices.append(random.randint(1, 10**4))
-      tickets = {}
-      for p in prices:
-        if maybe():
-          tickets[p] = random.randint(1, 10**5)
-        else:
-          tickets[p] = 0
-      self.events[x] = tickets
