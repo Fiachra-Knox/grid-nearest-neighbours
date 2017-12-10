7f2a8ef24bdeeec725e926501e54f3577ba487af
Fiachra Knox <fiachraknox@hotmail.com>
2017-12-10 22:46:57 +0000

    Add argument parsing for frontend

--- frontend.py
+++ frontend.py
@@ -5,19 +5,74 @@ from generate import generate_grid, generate_events
 from utils import save_grid_as_csv, save_events_as_csv
 from world import World
 
+parser = argparse.ArgumentParser(
+  description='creates a random world and finds nearest events')
+parser.add_argument(
+  '-s', '--seed', type=str, default='none',
+  help='use SEED as a seed for the random number generator')
+parser.add_argument(
+  '-mx', '--minx', type=int, default=-10,
+  help='use MINX as the least x-coordinate of the world (default -10)')
+parser.add_argument(
+  '-my', '--miny', type=int, default=-10,
+  help='use MINY as the least y-coordinate of the world (default -10)')
+parser.add_argument(
+  '-Mx', '--maxx', type=int, default=10,
+  help='use MAXX as the greatest x-coordinate of the world (default 10)')
+parser.add_argument(
+  '-My', '--maxy', type=int, default=10,
+  help='use MAXY as the greatest y-coordinate of the world (default 10)')
+parser.add_argument(
+  '-r', '--return_length', type=int, default=5,
+  help='return up to RETURN_LENGTH responses to each query (default 5)')
+parser.add_argument(
+  '-g', '--grid', type=str, default='temp_grid',
+  help='write grid data to GRID.csv (default temp_grid.csv)')
+parser.add_argument(
+  '-e', '--events', type=str, default='temp_events',
+  help='write event data to EVENTS.csv (default temp_events.csv)')
+args = parser.parse_args()
+
 # Grid and event parameters
-min_coords = (-10, -10)
-size = (21, 21)
+min_coords = (args.minx, args.miny)
+size = (args.maxx - args.minx + 1, args.maxy - args.miny + 1)
+for i in range(2):
+  if size[i] <= 0:
+    print('Warning: Grid has invalid size; using default instead.')
+    size[i] = 21
 num_events = size[0]*size[1]
 
 # Filenames (extension must always be .csv or .txt)
-grid_filename = 'temp_grid.csv'
-events_filename = 'temp_events.csv'
+for forb in '#%&{}\\<>*?/ $!\'\":@\n\t':
+  if forb in args.grid:
+    print('Warning: Grid filename has invalid characters. Using default.')
+    grid_filename = 'temp_grid.csv'
+    break
+else:
+  if len(args.grid) > 27:
+    print('Warning: Grid filename is too long and will be truncated.')
+    grid_filename = args.grid[:27] + '.csv'
+  else:
+    grid_filename = args.grid + '.csv'
 
-# Number of events to print in response to each user query
-k = 5
+for forb in '#%&{}\\<>*?/ $!\'\":@\n\t':
+  if forb in args.events:
+    print('Warning: Events filename has invalid characters. Using default.')
+    events_filename = 'temp_events.csv'
+    break
+else:
+  if len(args.events) > 27:
+    print('Warning: Events filename is too long and will be truncated.')
+    events_filename = args.events[:27] + '.csv'
+  else:
+    events_filename = args.events + '.csv'
 
-# Common list of event numbers is generated seperately
+
+# Accept a seed if given, for testing purposes
+if args.seed != 'none':
+  random.seed(args.seed)
+
+# Common list of event numbers for grid and events
 event_nos = random.sample(range(10**9), k=num_events)
 
 # Create grid and events files to feed to World
@@ -31,24 +86,22 @@ del events
 # Create world and answer user queries
 w = World(grid_filename, events_filename, min_coords=min_coords)
 while True:
-  inp = input('Please Input Coordinates: ')
+  inp = input('\nPlease Input Coordinates: ')
   print('')
   if inp in ['exit', 'quit', 'logout']:
     break
   inp = inp.split(',')
   if len(inp) != 2:
     print('Error: Expected 2 coordinates, received %d.' % len(inp))
-    print('Please Input Exactly Two Coordinates.\n')
+    print('Please Input Exactly Two Coordinates.')
     continue
   try:
     x = [int(s) for s in inp]
   except ValueError:
     print('Error: Coordinates must be integers.')
-    print('Please Input Integer Coordinates.\n')
+    print('Please Input Integer Coordinates.')
     continue
   for i in range(2):
     if x[i] < min_coords[i] or x[i] >= min_coords[i] + size[i]:
-      print('Warning: Coordinate %d is out of bounds.' % x[i])
-  print('')
-  w.print_nearest_events(x, k=k)
-  print('')
+      print('Warning: Coordinate %d is out of bounds.\n' % x[i])
+  w.print_nearest_events(x, k=args.return_length)
